2623c7166df985c79dae1e34d1d50ab4f9f9c91f
Ken <odessa5d4@gmail.com>
2021-10-24 20:06:49 +0900

    shader updated.

---
+++ PBRSample_Metal/resources/Metal.hlsl
@@ -0,0 +1,52 @@
+cbuffer Constants : register(b0)
+{
+    float4x4 gWorld;
+    float4x4 gView;
+    float4x4 gProjection;
+    
+    float4 gLightPosition;
+    float4 gLightColor;
+}
+
+struct VertexInput
+{
+    float3 Position : POSITION0;
+    float4 Normal : NORMAL0;
+    float2 Texcoord : TEXCOORD0;
+};
+
+struct PixelInput
+{
+    float4 Position : SV_Position;
+    float4 Normal : TEXCOORD0;
+    float2 Texcoord : TEXCOORD1;
+    float4 WorldPosition : TEXCOORD2;
+
+};
+
+PixelInput vert(VertexInput input)
+{
+    PixelInput output = (PixelInput) 0;
+    
+    output.Position = mul(float4(input.Position, 1.0f), gWorld);
+    output.WorldPosition = output.Position;
+    output.Position = mul(output.Position, gView);
+    output.Position = mul(output.Position, gProjection);
+    
+    output.Normal = input.Normal;
+    output.Texcoord = input.Texcoord;
+    
+	return output;
+}
+
+float4 frag(PixelInput input) : SV_Target0
+{
+    float4 color;
+    
+    float4 lightDir = normalize(gLightPosition - input.WorldPosition);
+    
+    float intensity = gLightPosition.w;
+    float diffuse = dot(lightDir, input.Normal) * gLightColor;
+    
+    return diffuse.xxxx;
+}

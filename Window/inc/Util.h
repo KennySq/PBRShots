27933b4b8cb0782fd8e9b1a433379d31dadcb21e
Kenny91 <odessa5d4@gmail.com>
2021-10-25 17:31:31 +0900

    GetWorkingDirectory() function added.

--- Window/inc/Util.h
+++ Window/inc/Util.h
@@ -11,3 +11,31 @@ static void Throw(HRESULT result)
 		throw std::runtime_error("");
 	}
 }
+
+static std::wstring GetWorkingDirectoryW()
+{
+	static constexpr unsigned int MAX_STR = 512;
+	wchar_t buffer[MAX_STR];
+
+	GetModuleFileNameW(nullptr, buffer, MAX_STR);
+
+	std::wstring path(buffer);
+
+	path = path.substr(0, path.find_last_of('\\') + 1);
+
+	return path;
+}
+
+static std::string GetWorkingDirectoryA()
+{
+	static constexpr unsigned int MAX_STR = 512;
+	char buffer[MAX_STR];
+
+	GetModuleFileName(nullptr, buffer, MAX_STR);
+
+	std::string path(buffer);
+
+	path = path.substr(0, path.find_last_of('\\') + 1);
+
+	return path;
+}
